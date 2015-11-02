a36b464a80be78b1c2a827586d5b55fc62e4d61f
lyes kherbiche <kerbiche@gmail.com>
2015-11-02 15:16:58 +0100

    make mvc strategie

---
+++ syndic-client-web/src/main/java/org/syndic/client/web/home/controller/Home.java
@@ -0,0 +1,25 @@
+package org.syndic.client.web.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author LYES KHERBICHE
+ *
+ */
+@Controller
+public class Home {
+	
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome() {
+		return "welcomePage";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "loginPage";
+	}
+
+}
