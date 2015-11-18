e8150cbd618428ef5499d75644db784e75328b9a
balaasri <brahim.px@gmail.com>
2015-11-18 11:11:58 +0100

    brahim

--- syndic-client-web/src/main/java/org/syndic/client/web/home/controller/Home.java
+++ syndic-client-web/src/main/java/org/syndic/client/web/home/controller/Home.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class Home {
-	
+
 	
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String welcome() {
