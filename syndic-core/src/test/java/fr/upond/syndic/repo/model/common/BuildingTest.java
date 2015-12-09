2620fca3a71f96975cdd7942420def54bf3ba894
leite <leite.maleck@outlook.com>
2015-12-09 14:38:40 +0100

    add BuildingTest

---
+++ syndic-core/src/test/java/fr/upond/syndic/repo/model/common/BuildingTest.java
@@ -0,0 +1,33 @@
+package fr.upond.syndic.repo.model.common;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by leite_2 on 09/12/2015.
+ */
+public class BuildingTest extends TestCase {
+
+    public void testGetId() throws Exception {
+
+    }
+
+    public void testSetId() throws Exception {
+
+    }
+
+    public void testGetName() throws Exception {
+
+    }
+
+    public void testSetName() throws Exception {
+
+    }
+
+    public void testGetAddress() throws Exception {
+
+    }
+
+    public void testSetAddress() throws Exception {
+
+    }
+}
